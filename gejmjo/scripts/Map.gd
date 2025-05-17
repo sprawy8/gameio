60aee2a813a71938a261506365019910fb7d31dc
Marcin Baszak <marcin.baszak5@gmail.com>
2025-05-17 11:59:46 +0200

    Added click detection and troop growing

--- gejmjo/scripts/Map.gd
+++ gejmjo/scripts/Map.gd
@@ -1,10 +1,35 @@
 extends Node2D
 
 @onready var territories := []
+var selected_territory = null
+
+#Growth of troops
+var growth_interval = 1.0 # seconds
+var growth_amount = 1
 
 func _ready():
 	territories = get_tree().get_nodes_in_group("territories")
 	assign_initial_owners()
+	var timer = Timer.new()
+	timer.wait_time = growth_interval
+	timer.one_shot = false
+	timer.autostart = true
+	add_child(timer)
+	timer.connect("timeout", Callable(self, "_on_growth_timer_timeout"))
+
+func _on_growth_timer_timeout():
+	for territory in get_tree().get_nodes_in_group("territories"):
+		territory.troop_count += growth_amount
+		territory.update_display()
+		
+func on_territory_clicked(territory):
+	if selected_territory:
+		selected_territory.set_selected(false)
+
+	selected_territory = territory
+	selected_territory.set_selected(true)
+
+	print("Selected territory with owner:", selected_territory.owner_id)
 
 func assign_initial_owners():
 	var player_colors = [
--- gejmjo/scripts/Territory.gd
+++ gejmjo/scripts/Territory.gd
@@ -5,20 +5,23 @@ extends Area2D
 
 var default_color = Color(0.8, 0.8, 0.8)
 var selected_color = Color(1, 1, 0)
+var is_selected: bool = false
 
 @onready var poly = $Polygon2D
 @onready var label = $Label
+@onready var collision: CollisionPolygon2D = $CollisionPolygon2D
 
 func _ready():
 	# Make sure the territory is in the group, so Map.gd finds it
 	add_to_group("territories")
-
+	#collision.input_pickable = true
 	# Give it a default color for testing (will be overwritten later)
-	$Polygon2D.color = Color.RED
-	
+	#$Polygon2D.color = Color.RED	
 	update_display()
 
 func update_display():
+	#Highlight selected
+	#poly.color = selected_color if is_selected else default_color
 	#poly.color = default_color
 	label.text = str(troop_count)
 
@@ -29,4 +32,10 @@ func assign_owner(id: int, color: Color):
 
 
 func set_selected(selected: bool):
-	poly.color = selected_color if selected else default_color
+	is_selected = selected
+	update_display()
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		get_parent().on_territory_clicked(self)
